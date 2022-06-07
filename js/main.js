ffa8076219c60662f940cb125cb14edc1e960bfb
Victor Polyakov <al-31f@yandex.ru>
2022-06-07 22:04:35 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function getRandomInt(min, max) {
+  if (min < 0 || max < 0) {
+    throw Error('Недопустимый диапазон! Введите числа от нуля и больше!');
+  }
+  if (max < min) {
+    const memory = max;
+    max = min;
+    min = memory;
+  }
+  if (min === max) {
+    throw Error('Недопустимый диапазон! Необходимо расширить диапазон')
+ }
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+getRandomInt(0,5);
+
+
+
+function lengthTest(string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  }
+    return false;
+}
+
+lengthTest('Не было Галiлея i Боба Марлея, не было Сальвадора Далi', 60);
