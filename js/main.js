d6adbf313969760497190f59e44b895dea85a8da
Victor Polyakov <al-31f@yandex.ru>
2022-07-15 20:22:58 +0300

    подключение фильтров и ресайза загружаемой фотографии

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ import {genPosts} from './data.js';
 import {getPictures} from './pictures.js';
 import './modal.js';
 import './form-validation.js';
-
+import './photo-filters.js';
+import './photo-resize.js';
 
 //создание постов
 const posts = genPosts();
 
 //прорисовка постов на сайте
 getPictures(posts);
-
